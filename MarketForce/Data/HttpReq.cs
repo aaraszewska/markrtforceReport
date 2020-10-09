0e1b43169875bdd22b6d93505ee618f1fe8a421e
Anna <aaraszewska@gmail.com>
2020-10-09 22:12:18 +0100

    second commit

---
+++ MarketForce/Data/HttpReq.cs
@@ -0,0 +1,71 @@
+﻿using MarketForce.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketForce.Data
+{
+   public class HttpReq
+    {
+        private string BaseAddres = "https://dev-kfgfly.marketforce.com/api/api/report";
+
+
+        public async Task MakeString()
+        {
+            try
+            {
+
+
+                var getData = JsonConvert.DeserializeObject<RequestData>(await TestGet());
+            }
+            catch (Exception ex)
+            {
+                var a = ex.Message.ToString();
+            }
+        }
+
+        public async Task<string>  TestGet()
+        {
+           
+            string testRequest = "";
+            try
+            {
+
+                var request = HttpWebRequest.CreateHttp(BaseAddres + "");
+                request.Method = WebRequestMethods.Http.Get;
+                request.Headers.Add("Authorization: bc4b0be2fdd660c46df39aa418423b6d");
+                request.Accept = "application/json; charset=utf-8";
+
+                await Task.Factory.FromAsync<WebResponse>(request.BeginGetResponse, request.EndGetResponse, null)
+                    .ContinueWith(task =>
+                    {
+                        var respons = (HttpWebResponse)task.Result;
+                        if(respons.StatusCode == HttpStatusCode.OK)
+                        {
+                            StreamReader responseReader = new StreamReader(respons.GetResponseStream(), Encoding.UTF8);
+                            string responseData = responseReader.ReadToEnd();
+
+                            testRequest = responseData.ToString();
+                            responseReader.Close();
+
+                        }
+
+                        respons.Close();
+
+                    });
+            }
+            catch(Exception)
+            {
+                //just pass
+            }
+
+
+            return testRequest;
+        }
+    }
+}
---
+++ MarketForce/Model/RequestData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketForce.Model
+{
+  public  class RequestData
+    {
+       public string Location { get; set; }
+        public string value { get; set; }
+    }
+}

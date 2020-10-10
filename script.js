56d5bd014fd84c51745f1ce42c4a1a3473438ffc
Anna <aaraszewska@gmail.com>
2020-10-10 22:54:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,62 @@
 new Vue({
-	el: '#main',
-	
-	 data: {
-       
+  el: '#main',
 
-        result: "",
-        responseAvailable: false,
-        apiKey: 'bc4b0be2fdd660c46df39aa418423b6d'
+  data: {
 
+
+    result: "",
+    responseAvailable: false,
+    apiKey: 'bc4b0be2fdd660c46df39aa418423b6d'
+
+
+  },
+
+
+
+
+  methods: {
+
+    setLocation() {
+      this.setLocation = location;
+      this.getLocationData();
     },
 
-    methods: {
+    setValue(value) {
+      this.selected = value;
+      this.getValueData();
+    },
 
-        fetchAPIData() {
+    fetchAPIData() {
 
-            this.responseAvailable = false;
+      this.responseAvailable = false;
 
-            fetch("https://dev-kfgfly.marketforce.com/api/api/filterhelp", {
-                "method": "GET",
-                "headers": {
-                    "x-rapidapi-host": "dev-kfgfly.marketforce.com",
-                    "x-rapidapi-key": this.apiKey
-                    
-
-                }
-
-            })
-                .then(response => {
-                    if (response.ok) {
-                        return response.json()
-                    } else {
-                        alert("Server returned " + response.status + " : " + response.statusText);
-                    }
-                })
-                .then(response => {
-                    this.result = response.body;
-                    this.responseAvailable = true;
-                })
-                .then(data => console.log(data))
-                .catch(err => {
-                    console.log(err);
-                });
+      fetch("https://dev-kfgfly.marketforce.com/api/api/report", {
+          "method": "GET",
+          "headers": {
+            "x-rapidapi-host": "dev-kfgfly.marketforce.com",
+            "x-rapidapi-key": this.apiKey
 
 
-        }
+          }
+
+        })
+        .then(response => {
+          if (response.ok) {
+            return response.json()
+          } else {
+            alert("Server returned " + response.status + " : " + response.statusText);
+          }
+        })
+        .then(response => {
+          this.result = response.body;
+          this.responseAvailable = true;
+        })
+        .then(data => console.log(data))
+        .catch(err => {
+          console.log(err);
+        });
+
+
     }
+  }
 });
-	
